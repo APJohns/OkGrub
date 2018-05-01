94c9cc256eea1f32328e1c3558e8e9fc4bbdbcbf
Ash Johns <ashleypjohns@gmail.com>
2018-04-30 21:50:22 -0400

    Integrate Yelp API

--- okgrub/routes/index.js
+++ okgrub/routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const yelp = require('yelp-fusion');
+require('dotenv').config({path: 'variables.env'});
+
+const client = yelp.client(process.env.YELPKEY);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let searchRequest = {
+    term: 'barrington coffee',
+    location: 'boston, ma'
+  };
+
+  client.search(searchRequest).then(response => {
+    let top5 = response.jsonBody.businesses.slice(0, 5);
+
+    top5.forEach(place => {
+      console.log(place.name);
+    });
+  }).catch(e => {
+    console.log(e);
+  });
+
   res.render('index', { title: 'Express' });
 });
 
