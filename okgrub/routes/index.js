ec2b0048d0a6f209876df60efa130e104c4b4c9f
Ash Johns <ashleypjohns@gmail.com>
2018-05-09 15:57:26 -0400

    Implement controllers

--- okgrub/routes/index.js
+++ okgrub/routes/index.js
@@ -1,39 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
-const yelp = require('yelp-fusion');
-require('dotenv').config({path: 'variables.env'});
-
-const client = yelp.client(process.env.YELPKEY);
+const placeController = require('../controllers/placeController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OkGrub' });
-});
-
-router.post('/', (req, res, next) => {
-  let lat = req.body.lat.toFixed(10);
-  let lon = req.body.lon.toFixed(10);
-  console.log(req.body);
-
-  let searchRequest = {
-    term: 'coffee',
-    latitude: lat,
-    longitude: lon
-  };
-
-  client.search(searchRequest).then(response => {
-    let places = response.jsonBody.businesses.slice(0, 20);
-
-    places.forEach(place => {
-      console.log(place.name);
-    });
-
-    res.json(places);
-    
-  }).catch(e => {
-    console.log(e);
-  });
-});
+router.get('/', placeController.getPlaces);
+router.post('/', placeController.getPlaces);
 
 module.exports = router;
