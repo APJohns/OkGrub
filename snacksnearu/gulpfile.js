90a44178a97a600eda99706662b769d5b2fe3651
Ash Johns <ashleypjohns@gmail.com>
2018-09-16 17:59:24 -0400

    add Gulp

---
+++ snacksnearu/gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+gulp.task('autoprefixer', () => {
+	return gulp.src('./public/stylesheets/dev/*.css')
+		.pipe(postcss([ autoprefixer() ]))
+		.pipe(gulp.dest('./public/stylesheets'))
+});
